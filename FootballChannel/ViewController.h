c0ecffd9277de8e58a6ab63cc33a511439553b63
lanou3g <862760467@qq.com>
2016-05-18 09:22:07 +0800

    Initial Commit

---
+++ FootballChannel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FootballChannel
+//
+//  Created by lanou3g on 16/5/18.
+//  Copyright © 2016年 张志江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FootballChannel/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FootballChannel
+//
+//  Created by lanou3g on 16/5/18.
+//  Copyright © 2016年 张志江. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
